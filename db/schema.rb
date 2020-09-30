17c610e3df21edff3c84a2667661daff336db18e
vagrant <vagrant@localhost.localdomain>
2020-09-30 15:11:51 +0000

    (fix)add_column_cartproducts_products_productgenres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_093849) do
+ActiveRecord::Schema.define(version: 2020_09_30_150501) do
 
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_09_30_093849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "amount"
+    t.integer "product_id"
+    t.integer "customer_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -59,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_093849) do
     t.text "body"
     t.integer "price"
     t.boolean "sale_status"
+    t.integer "product_genre_id"
   end
 
   create_table "users", force: :cascade do |t|
