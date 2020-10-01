39890849c84dbde611e299285996784f0742dc57
vagrant <vagrant@localhost.localdomain>
2020-10-01 05:49:07 +0000

    (fix6)add_column_cartprducts_products_productsgenres

--- db/migrate/20200930155903_null_to_product.rb
+++ db/migrate/20200930155903_null_to_product.rb
@@ -8,7 +8,7 @@ class NullToProduct < ActiveRecord::Migration[5.2]
   	change_column :products, :sale_status, :boolean, default:"TRUE", null:false
 
     change_column :product_genres, :name, :string, null:false
-    change_column :product_genres, :genres_status, :boolean, default:"TURE", null:false
+    change_column :product_genres, :genres_status, :boolean, default:"TRUE", null:false
 
     change_column :cart_products, :product_id, :integer, null:false
     change_column :cart_products, :customer_id, :integer, null:false
